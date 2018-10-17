02c228ade4ceaeb420a2a42445e73f61777965f4
Jonathan Broche <jhonnybroche@gmail.com>
2018-10-17 19:04:53 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# libssh-scanner
+# libssh Scan
+
+## Introduction
+-----
+
+This is a python based script to identify hosts vulnerable to CVE-2018-10933. 
+
+The vulnerability is present on versions of libssh 0.6+ and was remediated by a patch present in libssh 0.7.6 and 0.8.4. For more details: [https://www.libssh.org/2018/10/16/libssh-0-8-4-and-0-7-6-security-and-bugfix-release/](https://www.libssh.org/2018/10/16/libssh-0-8-4-and-0-7-6-security-and-bugfix-release/)
+
+## Help
+-----
+
+```
+CVE-2018-10933 Scanner - Find vulnerable libssh services by Leap Security (@LeapSecurity)
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -v, --version         show program's version number and exit
+  -t TARGET, --target TARGET
+                        An ip address or new line delimited file containing
+                        IPs to banner grab for the vulnerability.
+  -p PORT, --port PORT  Set port of SSH service
+```
---
+++ libsshscan.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python2
+# CVE-2018-10933 Scanner by Leap Security (@LeapSecurity) https://leapsecurity.io
+
+
+import socket, argparse, sys, os
+
+class colors(object):
+    blue = "\033[1;34m"
+    normal = "\033[0;00m"
+    red = "\033[1;31m"
+    yellow = "\033[1;33m"
+    white = "\033[1;37m"
+
+def pstatus(ip, port, banner):
+  print "{blue}[*]{white} {ipaddr}:{port} is not vulnerable to authentication bypass ({banner})".format(blue=colors.blue, white=colors.normal, ipaddr=ip, port=port, banner=banner.strip()) 
+
+def ptimeout(ip, port, banner):
+  print "{red}[-]{white} {ipaddr}:{port} has timed out.".format(red=colors.red, white=colors.white, ipaddr=ip, port=port)
+
+def ppatch(ip, port, banner):
+  print "{blue}[*]{white} {ipaddr}:{port} has been patched ({banner})".format(blue=colors.blue, white=colors.normal, ipaddr=ip, port=port, banner=banner.strip()) 
+
+def pvulnerable(ip, port, banner):
+  print "{yellow}[!]{white} {ipaddr}:{port} is likely VULNERABLE to authentication bypass ({banner})".format(yellow=colors.yellow, white=colors.normal, ipaddr=ip, port=port, banner=banner.strip()) 
+
+
+def bannergrab(ip, port):
+  try:
+    s = socket.socket()
+    s.connect((ip,port))
+    s.settimeout(2.00000)
+    banner = s.recv(1024)
+    s.close()
+    return banner
+  except socket.timeout as e:
+    pass
+    return ""
+
+parser = argparse.ArgumentParser(description='CVE-2018-10933 Scanner - Find vulnerable libssh services by Leap Security (@LeapSecurity)', version="1.0.0")
+parser.add_argument('-t', '--target', help="An ip address or new line delimited file containing IPs to banner grab for the vulnerability.")
+parser.add_argument('-p', '--port', default=22, help="Set port of SSH service")
+
+if len(sys.argv) == 1:
+    parser.print_help()
+    sys.exit(1)
+
+args = parser.parse_args()
+ips, results = [], []
+
+if not args.target:
+    print "You didn't provide any work for me to do."
+    sys.exit(1)
+
+if args.target:
+      print "\nStatus: Searching for Vulnerable Hosts...\n"
+      if os.path.isfile(args.target): #if file add emails
+          with open (args.target) as f:
+              for line in f.readlines():
+                  ips.append(line.strip())
+      else: #if not scan the provided IP
+          ips.append(args.target.strip())
+
+
+for ip in ips:
+  results.append([ip, args.port, bannergrab(ip, args.port)])
+
+for result in results:
+  if result[2]:
+    if "libssh-0.6" in result[2]: #vulnerable
+      pvulnerable(result[0], result[1], result[2])
+    elif "libssh-0.7" in result[2]: #chjeck if patched
+      if int(result[2].split(".")[-1]) >= 6: #libssh is 0.7.6 or greater (patched)
+        ppatch(result[0], result[1], result[2])
+      else:
+        pvulnerable(result[0], result[1], result[2])
+    elif "libssh-0.8" in result[2]: #chjeck if patched
+      if int(result[2].split(".")[-1]) >= 4: #libssh is 0.8.4 or greater (patched)
+        ppatch(result[0], result[1], result[2])
+      else:
+        pvulnerable(result[0], result[1], result[2])
+    else: #not vulnerable
+      pstatus(result[0], result[1], result[2])
+  else:
+    ptimeout("[*] {}:{} timed out".format(result[0], result[1]))
+
+
+print "\nScanner Completed Successfully"
